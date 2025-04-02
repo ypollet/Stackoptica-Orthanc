2bb7f4bb04dd43772b6333a89c828c0a66084c97
Yann Pollet <pollet.yann1999@gmail.com>
2025-04-02 11:48:09 +0200

    Feat : efficient image loading

--- DicomizeStackopticaFiles.py
+++ DicomizeStackopticaFiles.py
@@ -12,7 +12,7 @@
 
 # the License, or (at your option) any later version.
 
-# 
+#
 
 # This program is distributed in the hope that it will be useful, but
 
@@ -26,7 +26,7 @@
 
 # You should have received a copy of the GNU Affero General Public License
 
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 from io import BytesIO
 import PIL
@@ -38,22 +38,21 @@ import json
 import requests
 import os
 
-path_to_project = "data/microscope"
-SOURCE = f'{path_to_project}/*.jpg'
-calib_file = f'{path_to_project}/stack.json'
+path_to_project = "data/Ptilonyssus/Ptilonyssus_PH"
+SOURCE = f"{path_to_project}/*.jpg"
+calib_file = f"{path_to_project}/stack.json"
 images = sorted(glob.glob(SOURCE))
 i = 0
 
 
-with open(calib_file, 'rb') as f:
+with open(calib_file, "rb") as f:
     stack_dict = json.load(f)
-    
-study_uid = pydicom.uid.generate_uid()
+
+study_uid = "1.2.826.0.1.3680043.8.498.28545477349567871815300287899857591652"
 series_uid = pydicom.uid.generate_uid()
 
-
 thumbnails_width = stack_dict["thumbnails_width"]
-thumbnails_height  = stack_dict["thumbnails_height"]
+thumbnails_height = stack_dict["thumbnails_height"]
 
 images = stack_dict["stack"]
 stacked_images = stack_dict["Stacked_images"]
@@ -66,54 +65,54 @@ for image_name in images:
     camera = os.path.basename(image)
     print(camera)
     ds = pydicom.dataset.Dataset()
-    ds.PatientName = 'Periglischrus^calcariflexus'
-    ds.PatientID = 'Periglischrus10275665'
-    ds.PatientBirthDate = '20200914'
-    ds.PatientSex = 'O'
+    ds.PatientName = "Ptilonyssus^Pycnonoti^Malayi"
+    ds.PatientID = "Ptilonyssus10275665"
+    ds.PatientBirthDate = "20200914"
+    ds.PatientSex = "O"
 
-    
-    ds.StudyDate = now.strftime('%Y%m%d')
-    ds.StudyTime = now.strftime('%H%M%S')
+    ds.StudyDate = now.strftime("%Y%m%d")
+    ds.StudyTime = now.strftime("%H%M%S")
 
-    ds.ImageType = [ 'ORIGINAL', 'PRIMARY' ]
+    ds.ImageType = ["ORIGINAL", "PRIMARY"]
     ds.UserContentLabel = camera
-    ds.Laterality = 'L'
-    ds.LossyImageCompression = '01'
-    ds.Modality = 'XC'  # External-camera photography
+    ds.Laterality = "L"
+    ds.LossyImageCompression = "01"
+    ds.Modality = "XC"  # External-camera photography
     ds.SOPClassUID = pydicom.uid.VLPhotographicImageStorage
     ds.SOPInstanceUID = pydicom.uid.generate_uid()
     ds.SeriesInstanceUID = series_uid
     ds.StudyInstanceUID = study_uid
     ds.PixelSpacing = image_data["PixelRatio"]
     ds.ImagePositionPatient = image_data["SlicePosition"]
-    
+
     try:
-        ds.ImageOrientationPatient = image_data["SliceOrientation"]  
+        ds.ImageOrientationPatient = image_data["SliceOrientation"]
     except:
-        #SliceOrientation not a key
+        # SliceOrientation not a key
         ds.ImageOrientationPatient = [1.0, 0.0, 0.0, 0.0, 1.0, 0.0]
-    
+
     ds.AccessionNumber = None
     ds.ReferringPhysicianName = None
     ds.SeriesNumber = None
     ds.StudyID = None
+    ds.StudyDescription = "Micro^Multi"
+    ds.SeriesDescription = "phase contrast (PH)"
     ds.InstanceNumber = None
     ds.Manufacturer = None
     ds.AcquisitionContextSequence = None
-    ds.InstanceNumber = i+1
-
+    ds.InstanceNumber = i + 1
 
     # Basic encapsulation of color JPEG
-    # https://pydicom.github.io/pydicom/stable/tutorials/pixel_data/compressing.html
+    # httpss://pydicom.github.io/pydicom/stable/tutorials/pixel_data/compressing.html
 
-    with open(image, 'rb') as f:
-        frames = [ f.read() ]
+    with open(image, "rb") as f:
+        frames = [f.read()]
         ds.PixelData = pydicom.encaps.encapsulate(frames)
 
     with PIL.Image.open(image) as im:
         ds.Rows = im.size[1]
         ds.Columns = im.size[0]
-        
+
         im.thumbnail((thumbnails_width, thumbnails_height))
         thumbnail_buffer = BytesIO()
         im.save(thumbnail_buffer, format="JPEG")
@@ -124,48 +123,51 @@ for image_name in images:
     ds.BitsStored = 8
     ds.HighBit = 7
     ds.PixelRepresentation = 0
-    ds.PhotometricInterpretation = 'YBR_FULL_422'
+    ds.PhotometricInterpretation = "YBR_FULL_422"
 
-    ds['PixelData'].VR = 'OB'  # always for encapsulated pixel data
+    ds["PixelData"].VR = "OB"  # always for encapsulated pixel data
     ds.is_little_endian = True
     ds.is_implicit_VR = False
 
     meta = pydicom.dataset.FileMetaDataset()
     meta.TransferSyntaxUID = pydicom.uid.JPEGBaseline8Bit
     ds.file_meta = meta
-        
-    out : BytesIO = BytesIO()
+
+    out: BytesIO = BytesIO()
     ds.save_as(out, write_like_original=False)
-    
-    response = requests.post('http://localhost:8042/instances', out.getvalue())
-    
+
+    response = requests.post("http://localhost:8042/instances", out.getvalue())
+
     response.raise_for_status()
-    
+
     uuid = response.json()["ID"]
     series_uuid = response.json()["ParentSeries"]
-    
-    r = requests.put(f'http://localhost:8042/instances/{uuid}/attachments/thumbnail', data=thumbnail_buffer.getvalue())
+
+    r = requests.put(
+        f"http://localhost:8042/instances/{uuid}/attachments/thumbnail",
+        data=thumbnail_buffer.getvalue(),
+    )
     i += 1
-    
+
 for stacked in stacked_images:
     print(stacked)
     image = f"{path_to_project}/{stacked_images[stacked]}"
     camera = os.path.basename(image)
     print(camera)
     ds = pydicom.dataset.Dataset()
-    ds.PatientName = 'Periglischrus^calcariflexus'
-    ds.PatientID = 'Periglischrus10275665'
-    ds.PatientBirthDate = '20200914'
-    ds.PatientSex = 'O'
+    ds.PatientName = "Ptilonyssus^Pycnonoti^Malayi"
+    ds.PatientID = "Ptilonyssus10275665"
+    ds.PatientBirthDate = "20200914"
+    ds.PatientSex = "O"
 
-    ds.StudyDate = now.strftime('%Y%m%d')
-    ds.StudyTime = now.strftime('%H%M%S')
+    ds.StudyDate = now.strftime("%Y%m%d")
+    ds.StudyTime = now.strftime("%H%M%S")
 
-    ds.ImageType = [ 'DERIVED', 'PRIMARY' ]
+    ds.ImageType = ["DERIVED", "PRIMARY"]
     ds.UserContentLabel = stacked
-    ds.Laterality = 'L'
-    ds.LossyImageCompression = '01'
-    ds.Modality = 'XC'  # External-camera photography
+    ds.Laterality = "L"
+    ds.LossyImageCompression = "01"
+    ds.Modality = "XC"  # External-camera photography
     ds.SOPClassUID = pydicom.uid.VLPhotographicImageStorage
     ds.SOPInstanceUID = pydicom.uid.generate_uid()
     ds.SeriesInstanceUID = series_uid
@@ -175,23 +177,24 @@ for stacked in stacked_images:
     ds.ReferringPhysicianName = None
     ds.SeriesNumber = None
     ds.StudyID = None
+    ds.StudyDescription = "Micro^Multi"
+    ds.SeriesDescription = "phase contrast (PH)"
     ds.InstanceNumber = None
     ds.Manufacturer = None
     ds.AcquisitionContextSequence = None
-    ds.InstanceNumber = i+1
-
+    ds.InstanceNumber = i + 1
 
     # Basic encapsulation of color JPEG
-    # https://pydicom.github.io/pydicom/stable/tutorials/pixel_data/compressing.html
+    # httpss://pydicom.github.io/pydicom/stable/tutorials/pixel_data/compressing.html
 
-    with open(image, 'rb') as f:
-        frames = [ f.read() ]
+    with open(image, "rb") as f:
+        frames = [f.read()]
         ds.PixelData = pydicom.encaps.encapsulate(frames)
 
     with PIL.Image.open(image) as im:
         ds.Rows = im.size[1]
         ds.Columns = im.size[0]
-        
+
         im.thumbnail((thumbnails_width, thumbnails_height))
         thumbnail_buffer = BytesIO()
         im.save(thumbnail_buffer, format="JPEG")
@@ -202,26 +205,28 @@ for stacked in stacked_images:
     ds.BitsStored = 8
     ds.HighBit = 7
     ds.PixelRepresentation = 0
-    ds.PhotometricInterpretation = 'YBR_FULL_422'
+    ds.PhotometricInterpretation = "YBR_FULL_422"
 
-    ds['PixelData'].VR = 'OB'  # always for encapsulated pixel data
+    ds["PixelData"].VR = "OB"  # always for encapsulated pixel data
     ds.is_little_endian = True
     ds.is_implicit_VR = False
 
     meta = pydicom.dataset.FileMetaDataset()
     meta.TransferSyntaxUID = pydicom.uid.JPEGBaseline8Bit
     ds.file_meta = meta
-        
-    out : BytesIO = BytesIO()
+
+    out: BytesIO = BytesIO()
     ds.save_as(out, write_like_original=False)
-    
-    response = requests.post('http://localhost:8042/instances', out.getvalue())
-    
+
+    response = requests.post("http://localhost:8042/instances", out.getvalue())
+
     response.raise_for_status()
-    
+
     uuid = response.json()["ID"]
     series_uuid = response.json()["ParentSeries"]
-    
-    r = requests.put(f'http://localhost:8042/instances/{uuid}/attachments/thumbnail', data=thumbnail_buffer.getvalue())
+
+    r = requests.put(
+        f"http://localhost:8042/instances/{uuid}/attachments/thumbnail",
+        data=thumbnail_buffer.getvalue(),
+    )
     i += 1
-    
--- python-plugin.py
+++ python-plugin.py
@@ -1,6 +1,6 @@
-# Stackoptica - 3D Viewer on calibrated images - Orthanc Plugin
+# Stackoptica - Viewer of stacked images - Orthanc Plugin
 
-# Copyright (C) 2024 Yann Pollet, Royal Belgian Institute of Natural Sciences
+# Copyright (C) 2024-2025 Yann Pollet, Royal Belgian Institute of Natural Sciences
 
 #
 
@@ -12,7 +12,7 @@
 
 # the License, or (at your option) any later version.
 
-# 
+#
 
 # This program is distributed in the hope that it will be useful, but
 
@@ -35,123 +35,144 @@ import orthanc
 
 ##############################################################################
 #                                                                            #
-#------------------------------- STACKOPTICA --------------------------------#
+# ------------------------------- STACKOPTICA --------------------------------#
 #                                                                            #
-##############################################################################      
+##############################################################################
+
 
 def compute_landmark(output, uri, **request):
-  if request['method'] == 'GET':
-    instanceId = request['groups'][0]
-    x = float(request["get"]["x"])
-    y = float(request["get"]["y"])
-    orthanc.LogWarning(f"Compute position of ({x};{y}) at {instanceId}")
-    
-    
-    tags = json.loads(orthanc.RestApiGet(f"/instances/{instanceId}/simplified-tags"))
-    
-    pixel_spacing = [float(x) for x in tags["PixelSpacing"].split('\\')]
-    image_position_patient = [float(x) for x in tags["ImagePositionPatient"].split('\\')]
-    
-    position = {
-        "x" : x*pixel_spacing[0] + image_position_patient[0],
-        "y" : y*pixel_spacing[1] + image_position_patient[1],
-        "z" : image_position_patient[2]
-    }
-  
-    output.AnswerBuffer(json.dumps(position, indent = 3), 'application/json')
-  else:
-    output.SendMethodNotAllowed('GET')
+    if request["method"] == "GET":
+        instanceId = request["groups"][0]
+        x = float(request["get"]["x"])
+        y = float(request["get"]["y"])
+        orthanc.LogWarning(f"Compute position of ({x};{y}) at {instanceId}")
 
-orthanc.RegisterRestCallback('/stackoptica/(.*)/position', compute_landmark)
+        tags = json.loads(
+            orthanc.RestApiGet(f"/instances/{instanceId}/simplified-tags")
+        )
+
+        pixel_spacing = [float(x) for x in tags["PixelSpacing"].split("\\")]
+        image_position_patient = [
+            float(x) for x in tags["ImagePositionPatient"].split("\\")
+        ]
+
+        position = {
+            "x": x * pixel_spacing[0] + image_position_patient[0],
+            "y": y * pixel_spacing[1] + image_position_patient[1],
+            "z": image_position_patient[2],
+        }
+
+        output.AnswerBuffer(json.dumps(position, indent=3), "application/json")
+    else:
+        output.SendMethodNotAllowed("GET")
+
+
+orthanc.RegisterRestCallback("/stackoptica/(.*)/position", compute_landmark)
 
 
 def get_response_image(instance) -> bytearray:
     return orthanc.RestApiGet(f"/instances/{instance}/content/7fe0-0010/1")
 
+
 def get_response_thumbnail(instance) -> bytearray:
     return orthanc.RestApiGet(f"/instances/{instance}/attachments/thumbnail/data")
 
+
 # send single image
 def image(output, uri, **request):
-  if request['method'] == 'GET':
-    instanceId = request['groups'][0]
-    orthanc.LogWarning(f"Request full image of {instanceId}")
-    try:
-      instanceId = request['groups'][0]
-      image_binary = get_response_image(instanceId)
-      output.AnswerBuffer(image_binary, 'image/jpeg')
-    except Exception as error:
-      orthanc.LogError(error)
-  else:
-    output.SendMethodNotAllowed('GET')
-  
-orthanc.RegisterRestCallback('/stackoptica/(.*)/full-image', image)
+    if request["method"] == "GET":
+        instanceId = request["groups"][0]
+        orthanc.LogWarning(f"Request full image of {instanceId}")
+        try:
+            instanceId = request["groups"][0]
+            image_binary = get_response_image(instanceId)
+            output.AnswerBuffer(image_binary, "image/jpeg")
+        except Exception as error:
+            orthanc.LogError(error)
+    else:
+        output.SendMethodNotAllowed("GET")
+
+
+orthanc.RegisterRestCallback("/stackoptica/(.*)/full-image", image)
+
 
 # send single image
 def thumbnail(output, uri, **request):
-  if request['method'] == 'GET':
-    instanceId = request['groups'][0]
-    orthanc.LogWarning(f"Request thumbnail image of {instanceId}")
-    try:
-      instanceId = request['groups'][0]
-      image_binary = get_response_thumbnail(instanceId)
-      output.AnswerBuffer(image_binary, 'image/jpeg')
-    except Exception as error:
-      orthanc.LogError(error)
-  else:
-    output.SendMethodNotAllowed('GET')
-  
-orthanc.RegisterRestCallback('/stackoptica/(.*)/thumbnail', thumbnail)
-  
+    if request["method"] == "GET":
+        instanceId = request["groups"][0]
+        orthanc.LogWarning(f"Request thumbnail image of {instanceId}")
+        try:
+            instanceId = request["groups"][0]
+            image_binary = get_response_thumbnail(instanceId)
+            output.AnswerBuffer(image_binary, "image/jpeg")
+        except Exception as error:
+            orthanc.LogError(error)
+    else:
+        output.SendMethodNotAllowed("GET")
+
+
+orthanc.RegisterRestCallback("/stackoptica/(.*)/thumbnail", thumbnail)
+
 
 # send images
 def images(output, uri, **request):
-  if request['method'] == 'GET':
-    seriesId = request['groups'][0]
-    orthanc.LogWarning(f"Request stackoptica camera images of {seriesId}")
-    try:
-      orthanc_dict = json.loads(orthanc.RestApiGet(f"/series/{seriesId}/instances-tags?simplify"))
-      
-      to_jsonify = {}
-      encoded_images = []
-      projections = dict()
-      stackedImages = dict()
-      height = 0
-      width = 0
-      for instance, tags in orthanc_dict.items():
+    if request["method"] == "GET":
+        seriesId = request["groups"][0]
+        orthanc.LogWarning(f"Request stackoptica camera images of {seriesId}")
         try:
-          if "ORIGINAL" in tags["ImageType"]:
-            print(f"start : {instance}")
-            width = tags["Columns"]
-            height = tags["Rows"]
-            encoded_images.append([instance, tags["UserContentLabel"]])
-            
-            orientation = np.array([float(x) for x in tags["ImageOrientationPatient"].split("\\")])
-            position = np.array([float(x) for x in tags["ImagePositionPatient"].split("\\")])
-            normal = np.cross(orientation[0:3], orientation[3:])
-            projections[instance] = np.dot(position, normal)
-          if "DERIVED" in tags["ImageType"]:
-            stackedImages[tags["UserContentLabel"]] = [instance, tags["UserContentLabel"]]
-        except Exception as error:
-          print(error)
-          continue
-      
-        
-      encoded_images.sort(key=lambda image_data: projections[image_data[0]])
-      to_jsonify["stackImages"] = encoded_images
-      to_jsonify["individualImages"] = stackedImages
-      to_jsonify["size"] = {
-        "width" : width,
-        "height" : height
-      }   
-      output.AnswerBuffer(json.dumps(to_jsonify), "application/json")
-    except ValueError as e:
-      orthanc.LogError(e)
-  else:
-    output.SendMethodNotAllowed('GET')
+            orthanc_dict = json.loads(
+                orthanc.RestApiGet(f"/series/{seriesId}/instances-tags?simplify")
+            )
 
-orthanc.RegisterRestCallback('/stackoptica/(.*)/images', images)
-extension = '''
+            to_jsonify = {}
+            encoded_images = []
+            projections = dict()
+            stackedImages = dict()
+            height = 0
+            width = 0
+            for instance, tags in orthanc_dict.items():
+                try:
+                    if "ORIGINAL" in tags["ImageType"]:
+                        print(f"start : {instance}")
+                        width = tags["Columns"]
+                        height = tags["Rows"]
+                        encoded_images.append(
+                            {"name": instance, "label": tags["UserContentLabel"]}
+                        )
+
+                        orientation = np.array(
+                            [
+                                float(x)
+                                for x in tags["ImageOrientationPatient"].split("\\")
+                            ]
+                        )
+                        position = np.array(
+                            [float(x) for x in tags["ImagePositionPatient"].split("\\")]
+                        )
+                        normal = np.cross(orientation[0:3], orientation[3:])
+                        projections[instance] = np.dot(position, normal)
+                    if "DERIVED" in tags["ImageType"]:
+                        stackedImages[tags["UserContentLabel"]] = {
+                            "name": instance,
+                            "label": tags["UserContentLabel"],
+                        }
+                except Exception as error:
+                    print(error)
+                    continue
+
+            encoded_images.sort(key=lambda image_data: projections[image_data[0]])
+            to_jsonify["stackImages"] = encoded_images
+            to_jsonify["individualImages"] = stackedImages
+            to_jsonify["size"] = {"width": width, "height": height}
+            output.AnswerBuffer(json.dumps(to_jsonify), "application/json")
+        except ValueError as e:
+            orthanc.LogError(e)
+    else:
+        output.SendMethodNotAllowed("GET")
+
+
+orthanc.RegisterRestCallback("/stackoptica/(.*)/images", images)
+extension = """
     const STACKOPTICA_PLUGIN_SOP_CLASS_UID = '1.2.840.10008.5.1.4.1.1.77.1.4'
     $('#series').live('pagebeforeshow', function() {
       var seriesId = $.mobile.pageData.uuid;
@@ -180,5 +201,5 @@ extension = '''
         });
       });
     });
-    '''
+    """
 orthanc.ExtendOrthancExplorer(extension)
